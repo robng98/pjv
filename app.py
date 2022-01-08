b550668146281d39e5519aa55ae8ce253ab1f1d1
robng <betonicacio10@gmail.com>
2022-01-08 07:10:07 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,73 @@
+import os
+from sys import path
+from flask import Flask, render_template, request, redirect, flash, url_for, send_file
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.secret_key = b'secret'
+
+BASE_PATH =  '/uploads'
+abs_path = os.path.abspath(BASE_PATH)
+ALLOWED_EXT = {'txt', 'pdf'}
+
+def allowed_files(filename: str) -> bool :
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXT
+
+
+def vamover(file):
+    for line in file:
+        flash(line)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files.get('file')
+
+        if file.filename == '':
+            flash('No file selected')
+            return redirect(request.url)
+
+        if file and allowed_files(file.filename):
+
+            #FUNCAO ONDE FICARA TODOS OS SCRIPTS DE POS-PROCESSAMENTO
+            script(file)
+
+            # filename = secure_filename(file.filename)
+            # file.save(os.path.join('C:/Users/beton/Downloads/ProjetoFlask/uploads', filename))
+            return redirect(url_for('download'))
+
+    return render_template('index.html')
+
+@app.route('/download', methods=['GET'])
+def download():
+    return send_file('uploads/temp.txt', mimetype='text', attachment_filename='Diagnostico.txt', as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+def script(file):
+    with open('uploads/temp.txt', 'w+') as dest:
+            for line in file:
+                line_dcd = line.decode('UTF-8')
+                line_dcd = line_dcd.replace('$ADC$,', '')
+                line_dcd = line_dcd.replace(',', ', ')
+                line_dcd = line_dcd.replace('#', ' ')
+                line_dcd = line_dcd.replace(',', ' ')
+                line_dcd = line_dcd.split()
+                # print(line_dcd)
+                line_num = [int(val) for val in line_dcd]
+                cont = 0
+
+                for val in line_num:
+                    cont = cont + val
+                
+                cont_str = str(cont) + '\n'  
+                dest.write(cont_str)
+    flash('File uploaded successfully')
